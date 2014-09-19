7d98b18f80dc23da2622e3b20d9e914fd4f47870
Gary Leong <gwleong@gmail.com>
2014-09-19 21:27:53 +0000

    unlock cookbook versions

--- metadata.rb
+++ metadata.rb
@@ -7,12 +7,22 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-depends "apt", "2.3.8"
-depends "build-essential", "2.0.0"
-depends "yum", "3.1.4"
-depends "yum-epel", "0.3.4"
-depends "chef_gem", "0.1.0"
+#depends "apt", "2.3.8"
+#depends "build-essential", "2.0.0"
+#depends "yum", "3.1.4"
+#depends "yum-epel", "0.3.4"
+#depends "chef_gem", "0.1.0"
+#
+##In Berksfile 
+#depends "ruby_build"
+#depends "rvm", "0.9.1" 
+#
+depends "apt"
+depends "build-essential"
+depends "yum"
+depends "yum-epel"
+depends "chef_gem"
 
 #In Berksfile 
 depends "ruby_build"
-depends "rvm", "0.9.1" 
+depends "rvm"
